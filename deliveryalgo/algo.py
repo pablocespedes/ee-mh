14a9d32e78b83d3b1c78db6f233de87cb2b49959
mt-huerta <mthuerta@gmail.com>
2015-01-19 03:01:25 -0500

    C part of algo implemented

--- deliveryalgo/algo.py
+++ deliveryalgo/algo.py
@@ -2,6 +2,33 @@ import order
 import deliveryslot
 import slotcollection
 
+def dynamicDeliveryEngine():
+    """
+        (C)
+            if above(2) _AND_ below(3) do not exist DeliverySlot.active = True 
+    """
+    h = 0
+    while h < sc.numHours:
+        c = 0
+        while c < sc.numCars:
+            """
+                (C)
+            """
+            #   if in 1st h slots _AND_ slot below is empty
+            if h == 0 and len(sc.slot2DList[c][h+1].listOfOrders) == 0:  
+                sc.slot2DList[c][h].active = True
+            
+            #   if in last h slots _AND_ slot above is empty
+            elif h == sc.numHours-1 and len(sc.slot2DList[c][h-1].listOfOrders) == 0:
+                sc.slot2DList[c][h].active = True
+
+            #   if slot above is empty _AND_ slot below is empty
+            elif len(sc.slot2DList[c][h-1].listOfOrders) == 0 and len(sc.slot2DList[c][h+1].listOfOrders) == 0:
+                sc.slot2DList[c][h].active = True
+
+            c = c + 1
+        h = h + 1
+   
 if __name__ == "__main__":
     #   new SlotCollection object filled with DeliverySlots
     sc = slotcollection.SlotCollection(12, 2)
@@ -14,22 +41,22 @@ if __name__ == "__main__":
     ao2 = order.Order("15 Park Ave")
     ao3 = order.Order("20 Park Ave")
     ao4 = order.Order("456 Park Ave")
-    ao4 = order.Order("456 5th Ave")
+    ao5 = order.Order("456 5th Ave")
 
     #   add Orders to a DeliverySlot(hour, car)
-    sc.addOrderToSlot(1, 1, ao1)
-    sc.addOrderToSlot(1, 1, ao2)
-    sc.addOrderToSlot(1, 1, ao3)
-    sc.addOrderToSlot(1, 1, ao4)
-    sc.addOrderToSlot(12, 2, ao4)
+    sc.addOrderToSlot(12, 1, ao1)
+    sc.addOrderToSlot(12, 2, ao2)
+    sc.addOrderToSlot(9, 1, ao3)
+    sc.addOrderToSlot(8, 2, ao4)
+    sc.addOrderToSlot(1, 2, ao5)
    
-    #   check that slots have deliveries in them 
-    print(sc.getSlot(1,1).printSlot())
-    print(sc.getSlot(12,2).printSlot())
+    #   pre-algo checks 
+    #print(sc.getSlot(1,1).printSlot())
+    #print(sc.getSlot(1,2).printSlot())
 
     #   see what the last order was for a slot
-    lo1 = sc.getSlot(1,1).getLastOrder()
-    print (lo1.address)
+    #lo1 = sc.getSlot(1,1).getLastOrder()
+    #print (lo1.address)
    
     """
         New User Order
@@ -41,9 +68,10 @@ if __name__ == "__main__":
     """
         Initiate slot logic
     """
-   #    if above (2) _AND_ below (3) do not exist DeliverySlot.active = True 
-
-    """
-        Check and test active/inactive states
-    """
+    dynamicDeliveryEngine()
  
+    """
+        Check slot logic outputs
+    """  
+    print(sc.getSlot(10,1).printSlot())
+    print(sc.getSlot(10,2).printSlot())
